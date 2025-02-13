b268057e75f7f7760b9cec7d7b364a823546258b
Ängla Juliusson <angla02@icloud.com>
2025-02-13 11:32:05 +0100

    Hämta json-fil och skriv ut i consolen

--- DT211G---Moment-2/src/js/main.js
+++ DT211G---Moment-2/src/js/main.js
@@ -3,3 +3,20 @@
 let coursename = [];
 let progression = [];
 let coursecode = [];
+
+// Funktionen körs när sidan har laddats
+window.onload = () => {
+    loadCourses();
+}
+
+// Funktion som hämtar information
+async function loadCourses() {
+    try {
+        const response = await fetch("https://webbutveckling.miun.se/files/ramschema_ht24.json"); // Hämta json-fil
+        const data = await response.json(); // Omvandlar till array eller objekt
+
+        console.table(data); // Skriver ut informationen i en tabell
+    } catch(error) {
+        console.error("error"); // Om något går fel skrivs felmeddelande ut
+    };
+}
