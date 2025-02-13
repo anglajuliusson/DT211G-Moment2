44340604e947cf188a1b752c6f8eca516dab1446
Ängla Juliusson <angla02@icloud.com>
2025-02-13 11:46:27 +0100

    Felmeddelande inläsning json-fil

--- DT211G---Moment-2/src/js/main.js
+++ DT211G---Moment-2/src/js/main.js
@@ -13,6 +13,9 @@ window.onload = () => {
 async function loadCourses() {
     try {
         const response = await fetch("https://webbutveckling.miun.se/files/ramschema_ht24.json"); // Hämta json-fil
+            if (!response.ok) {
+                throw new Error("Fel vid anslutning till data...") // Felmeddelande om json-filen inte kan läsas in korrekt
+            }
         const data = await response.json(); // Omvandlar till array eller objekt
 
         console.table(data); // Skriver ut informationen i en tabell
